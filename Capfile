69e79d814cc544fe74b1ee1ff99d06b0ddcd2727
Sumit Dey <sumit@curvetomorrow.com.au>
2020-02-21 11:55:54 +0530

    Cap

--- Capfile
+++ Capfile
@@ -14,8 +14,8 @@ require 'capistrano/rbenv'
 require 'capistrano/passenger'
 # require 'whenever/Capistrano'
 # require 'capistrano/chruby'
-# require 'capistrano/rails/assets'
-# require 'capistrano/rails/migrations'
+require 'capistrano/rails/assets'
+require 'capistrano/rails/migrations'
 
 # Load custom tasks from `lib/capistrano/tasks` if you have any defined
 Dir.glob('lib/capistrano/tasks/*.rake').each { |r| import r }
--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/Download
 append :linked_files, 'config/database.yml'
 
 # Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
