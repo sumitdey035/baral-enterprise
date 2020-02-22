5554fc76708c23d1c1bbe0756b78222dc7ad7244
Sumit Dey <sumit@curvetomorrow.com.au>
2020-02-22 10:06:20 +0530

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,13 @@
-# config valid for current version and patch releases of Capistrano
 lock '~> 3.12.0'
 
-# server '172-31-39-160', port: 22, roles: [:web, :app, :db], primary: true
 server '13.127.91.212', port: 22, roles: %i[web app db], primary: true
 
 set :application, 'baral-enterprise'
-set :repo_url, 'https://github.com/sumitdey035/baral-enterprise.git'
-set :git_https_username, 'sumit.dey.035@gmail.com'
-set :git_https_password, 'Luvalomistu035'
-# set :repo_url, 'git@github.com:sumitdey035/baral-enterprise.git'
-set :user, 'ubuntu' #server user
+# set :repo_url, 'https://github.com/sumitdey035/baral-enterprise.git'
+# set :git_https_username, 'sumit.dey.035@gmail.com'
+# set :git_https_password, 'Luvalomistu035'
+set :repo_url, 'git@github.com:sumitdey035/baral-enterprise.git'
+set :user, 'ubuntu' # server user
 set :rbenv_ruby, '2.5.3'
 
 # Don't change these unless you know what you're doing
@@ -20,43 +18,14 @@ set :stage, :production
 set :deploy_via, :remote_cache
 set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 
-set :rvm_map_bins, %w{gem rake ruby rails bundle}
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/Downloads/baral_enterprise.pem) }
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
 append :linked_files, 'config/database.yml', 'config/master.key'
-
-# Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
 set :keep_releases, 5
 
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/Downloads/baral_enterprise.pem) }
 
+
+# Puma configurations
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
@@ -72,51 +41,14 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true
 set :puma_preload_app, false
 
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-# namespace :deploy do
-#   desc "Make sure local git is in sync with remote."
-#   task :check_revision do
+# namespace :puma do
+#   desc 'Create Directories for Puma Pids and Socket'
+#   task :make_dirs do
 #     on roles(:app) do
-#       unless `git rev-parse HEAD` == `git rev-parse origin/master`
-#         puts "WARNING: HEAD is not the same as origin/master"
-#         puts "Run `git push` to sync changes."
-#         exit
-#       end
+#       execute "mkdir #{shared_path}/tmp/sockets -p"
+#       execute "mkdir #{shared_path}/tmp/pids -p"
 #     end
 #   end
 #
-#   desc 'Initial Deploy'
-#   task :initial do
-#     on roles(:app) do
-#       before 'deploy:restart', 'puma:start'
-#       invoke 'deploy'
-#     end
-#   end
-#
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       invoke 'puma:restart'
-#     end
-#   end
-#
-#   before :starting,     :check_revision
-#   after  :finishing,    :compile_assets
-#   after  :finishing,    :cleanup
-#   after  :finishing,    :restart
+#   before :start, :make_dirs
 # end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
