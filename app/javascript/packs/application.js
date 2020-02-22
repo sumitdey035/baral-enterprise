cbfc6f120c7710b91dba22ce856bcebf7889a9ac
Sumit Dey <sumit@curvetomorrow.com.au>
2020-02-22 11:01:56 +0530

    add pack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import 'bootstrap'
+import './src/application.scss'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("packs/scripts")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +18,26 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+  /*!
+      * Start Bootstrap - SB Admin v6.0.0 (https://startbootstrap.com/templates/sb-admin)
+      * Copyright 2013-2020 Start Bootstrap
+      * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-sb-admin/blob/master/LICENSE)
+      */
+$( document ).on('turbolinks:load', function() {
+  "use strict";
+
+  // Add active state to sidbar nav links
+  var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+  $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
+    if (this.href === path) {
+      $(this).addClass("active");
+    }
+  });
+
+  // Toggle the side navigation
+  $("#sidebarToggle").on("click", function(e) {
+    e.preventDefault();
+    $("body").toggleClass("sb-sidenav-toggled");
+  });
+});
